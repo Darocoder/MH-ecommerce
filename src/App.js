47faa88a0cbee393eb095bb9d481c315b53cff71
Daro <Daro@iMac-de-Malaga.local>
2023-03-07 22:28:36 -0300

    hice cambios en el css e intenté cambiar el logo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto de e-commerce con react js
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tienda online
         </a>
       </header>
     </div>
